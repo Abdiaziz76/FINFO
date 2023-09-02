a2eca1b8a7adf239ee436ed075d8b8c5eef9d563
scaarifintl <scaarifintl@gmail.com>
2023-09-02 11:04:17 +0300

    set us simple Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/Landing'
+import HomePage from './pages/HomePage'
 
 function App() {
 
 
   return (
     <div className='w-full h-full'>
-      <LandingPage />
+     <Routes>
+        <Route path="/" element={ <LandingPage /> } />
+        <Route path="/home" element={ <HomePage /> } />
+     </Routes>
     </div>
   )
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      Welcome to Finfo!
+    </div>
+  )
+}
+
+export default HomePage
